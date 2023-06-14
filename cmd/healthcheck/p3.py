6afe2463957be31bfd02dceef5856fdfa362ed8a
Sants <51303087+yon3zu@users.noreply.github.com>
2023-06-14 14:04:49 +0800

    Update p3.py

--- cmd/healthcheck/p3.py
+++ cmd/healthcheck/p3.py
@@ -1,22 +1,30 @@
-import requests,os
-import time,re
+import requests
+import os
+import time
+import re
 from bs4 import BeautifulSoup
 from multiprocessing.dummy import Pool as ThreadPool
-from colorama import Fore,Style, init
+from colorama import Fore, Style, init
+
 init(autoreset=True)
 
 def Main():
-	os.system('cls' if os.name == 'nt' else 'clear')
-	print "{} xReverse V9  | {}#No_Identity - @xxyz4".format(Fore.CYAN,Fore.GREEN)
-	print "\t{} IP Reverse From 3 {}Server !\n".format(Fore.GREEN,Fore.YELLOW)
-	print "{} Server 1 {}Active !".format(Fore.CYAN,Fore.GREEN)
-	print "{} Server 2 {}Active !".format(Fore.CYAN,Fore.GREEN)
-	print "{} Server 3 {}Problem !\n".format(Fore.CYAN,Fore.RED)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("{} xReverse V9  | {}#No_Identity - @xxyz4".format(Fore.CYAN, Fore.GREEN))
+    print("\t{} IP Reverse From 3 {}Server !\n".format(Fore.GREEN, Fore.YELLOW))
+    print("{} Server 1 {}Active !".format(Fore.CYAN, Fore.GREEN))
+    print("{} Server 2 {}Active !".format(Fore.CYAN, Fore.GREEN))
+    print("{} Server 3 {}Active !\n".format(Fore.CYAN, Fore.GREEN))
 
-	try:
-		list = raw_input("TRIAL IS END, IF YOU WANT TO BUY CONTACT telegram @xxyz4 ")
-	except:
-		pass
+    try:
+        list = input("MAINTENANCE ")
+        url = open(list, 'r').read().splitlines()
+        THREAD = input("root@youez[thread]:~# ")
+        pp = ThreadPool(int(THREAD))
+        pr = pp.map(revip, url)
+    except:
+        pass
+
 
 if __name__ == '__main__':
-	Main()
+    Main()
